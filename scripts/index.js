c68a75dce1e73ea861a7c01cfb8e0b00eb52f9d9
Some Name <thatspock@gmail.com>
2024-04-15 18:34:16 +0300

    feat: implement create and delete cards function

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardsContainer = document.querySelector('.places__list');
+
 
 // @todo: Функция создания карточки
+function createCard(cardData, deleteCard) {
+  const clonedFragment = cardTemplate.cloneNode(true);
+  const cardElement = clonedFragment.querySelector('.card');
+
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = cardData.name;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+
+  cardElement.querySelector('.card__delete-button').addEventListener('click', () => {
+    deleteCard(cardElement);
+  });
+
+  return cardElement;  
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove(); 
+}
+
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(card => {
+  const cardElement = createCard(card, deleteCard);
+  cardsContainer.append(cardElement); 
+});
