9c72a4e36d9b22c62982455f458cbe9943cdd4d3
Some Name <thatspock@gmail.com>
2024-05-08 23:00:45 +0300

    feat: open/close modal window by esc and x

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+// import { pop } from 'core-js/core/array';
 import { initialCards } from './cards.js';
 import './pages/index.css';
 
 const cardTemplate = document.querySelector('#card-template').content;
 const cardsContainer = document.querySelector('.places__list');
+
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
+const card = document.querySelector('.card');
+const popupNew = document.querySelector('.popup_type_new-card');
+const popupEdit = document.querySelector('.popup_type_edit');
+const popupImage = document.querySelector('.popup_type_image');
+const popupClose = document.querySelector('.popup__close');
 
 // @todo: Функция создания карточки
 function createCard(cardData, deleteCard) {
@@ -29,3 +38,31 @@ initialCards.forEach(card => {
   const cardElement = createCard(card, deleteCard);
   cardsContainer.append(cardElement); 
 });
+
+function openPopup(popup) {
+  popup.classList.add('popup_is-opened');
+  document.addEventListener('keydown', closeOnEsc);
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_is-opened');
+  document.removeEventListener('keydown', closeOnEsc);
+}
+
+editButton.addEventListener('click', () => {
+  openPopup(popupEdit);
+});
+
+popupClose.addEventListener('click', () => {
+  const openedPopup = document.querySelector('.popup_is-opened');
+  closePopup(openedPopup);
+})
+
+function closeOnEsc(event) {
+  if (event.key === 'Escape') {
+    const openedPopup = document.querySelector('.popup_is-opened');
+    if (openedPopup) {
+      closePopup(openedPopup);
+    }
+  }
+}
